5e2f85d8d15fd9ba0ae520128f8ebf3efeb5d864
arman-asryan-CS30 <7014809@spsd.sk.ca>
2026-02-13 12:15:16 -0600

    bullet

--- FIrst Assignment/sketch.js
+++ FIrst Assignment/sketch.js
@@ -4,13 +4,26 @@
 
 //---------------Global Variables----------------------
 
+let currentBack = 0;
 
-function drawClouds(x,y){
+//---------------First Background--------------------------------
+function drawClouds1(x,y){
   stroke(201, 201, 197)
   fill(201, 201, 197)
-  circle(x,y,50)
-  circle(x-40,y,50)
-  circle(x-20,y+30,60)
+  circle(x,y-10,50)
+  circle(x-40,y-10,50)
+  circle(x-20,y+20,60)
+  ellipse(x+20,y+y/25,70,40)
+  ellipse(x-60,y+y/25,70,40)
+}
+
+function drawClouds2(x,y){
+  stroke(201, 201, 197)
+  fill(201, 201, 197)
+  circle(x,y-10,50)
+  circle(x-40,y-10,50)
+  circle(x-40,y+10,60)
+  circle(x,y+10,60)
   ellipse(x+20,y+y/25,70,40)
   ellipse(x-60,y+y/25,70,40)
 }
@@ -24,17 +37,78 @@ function drawBackground1(){
   circle(windowWidth-windowWidth/20,20,100)
   
   //Clouds
-  let cloudX = windowWidth;
-  let cloudY = random(windowHeight);
-  console.log(cloudY)
-  drawClouds(50,cloudY)
+  drawClouds1(windowWidth-100,windowHeight/4)
+  drawClouds1(windowWidth-300,windowHeight/2)
+  drawClouds1(windowWidth-550,windowHeight/3)
+  drawClouds1(windowWidth-500,windowHeight-300)
+  drawClouds1(windowWidth-760,windowHeight-450)
   
+  drawClouds2(windowWidth/2,windowHeight/10)
+  drawClouds2((windowWidth*3)/4,700)
 }
+
+//-----------------------Second Background---------------------------------
+
+
+//----------------Character-------------------------------------------
+
+function jet(x,y) {
+  //Fuselage
+  fill(0)
+  stroke(0)
+  ellipse(x,y,100,20)
+  fill(0)
+  stroke(0)
+
+  //Wings and vertical stabilizer
+  triangle(x,y,x-45,y-20,x-45,y+20)
+  triangle(x+40,y,x,y-40,x,y+40)
+}
+
+//Bullets
+function bullets(x,y) {
+  fill(255,255,0)
+  stroke(255,255,0)
+  line(x,y,x+100,y)
+}
+
+//-------------------Actions----------------------------
+
+//Background changing
+function mousePressed() {
+  if (mouseButton === "center") {
+    currentBack = currentBack + 1;
+  }
+
+  if (currentBack > 4) {
+    currentBack = 0
+  }
+}
+
+
+//Shooting
+function keyPressed() {
+  if (key === " ") {      
+    let bulletX = mouseX ;
+    let bulletY = mouseY;
+    bullets(bulletX,bulletY)    
+  }
+}
+
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  drawBackground1()
+  if (currentBack === 0) {
+    drawBackground1()
+  }
+  else{
+    background(255)
+  }
+  
+  jet(mouseX,mouseY)
 }
