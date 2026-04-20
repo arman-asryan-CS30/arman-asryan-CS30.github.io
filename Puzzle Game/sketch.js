d3cba6c1f9a9d8cdea75e8c82b2fe3f80dc44cc8
arman-asryan-CS30 <7014809@spsd.sk.ca>
2026-04-20 00:12:11 -0600

    Final Puzzle

--- Puzzle Game/sketch.js
+++ Puzzle Game/sketch.js
@@ -2,8 +2,9 @@
 // Arman Asryan
 // Apr 17 / 2026
 
+// Global Variables
 
-
+let shape;
 let grid = [
   [],
   [],
@@ -22,10 +23,13 @@ function setup() {
   setGrid(grid)
   row = grid.length;
   column = grid[0].length;
-  createCanvas(column * tileSize, row*tileSize);
+  createCanvas(column * tileSize, row*tileSize); //Create the canvas after the grid has been populated
+  shape = "cross" //Initial shape
+  
 }
 
 function setGrid(grid) {
+  //Populate the each row with either black or white randomly
   for(let row of grid){
     for (let i = 1; i <= 6; i++) {
       row.push(color[Math.round(random(0,1))])
@@ -34,27 +38,66 @@ function setGrid(grid) {
 }
 
 
-
 function draw() {
   background(250);
   renderGrid()
   overlay()
+  if(checkWin()) text("You Won !",width/2, height/2)
 }
+
+function checkWin() {
+  let first = grid[0][0];
+
+  //Checks every tile and returns false
+  //Even if only one of the tiles is different
+  //Than the first one
+  for (let y = 0; y < row; y++) {
+    for (let x = 0; x < column; x++) {
+      if (grid[y][x] !== first) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
 
 function overlay() {
   let x = getCurrentX()
   let y = getCurrentY()
+  
 
- 
+  if (!keyIsDown(SHIFT)) { //If shift isnt down then draw a cross or a square as overlay
+    if (shape === "cross") {
+    fill("Green")
+    square(x * tileSize,y* tileSize, tileSize)
+    square(x * tileSize,(y+1)* tileSize, tileSize)
+    square((x-1) * tileSize,y* tileSize, tileSize)
+    square(x * tileSize,(y-1)* tileSize, tileSize)
+    square((x+1) * tileSize,y* tileSize, tileSize)
+  }else{
+    fill("Green")
+    square(x * tileSize,y* tileSize, tileSize)
+    square(x * tileSize,(y-1)* tileSize, tileSize)
+    square((x-1) * tileSize,y* tileSize, tileSize)
+    square((x-1) * tileSize,(y-1)* tileSize, tileSize)
+  }
+  }else{ //Draw just one tile if shift is down
+    fill("Green")
+    square(x * tileSize,y* tileSize, tileSize)
+  }
  
 }
 
 function flip(x,y) {
+  //Check the color of the tile and changes it to the opposite color
   if (grid[y][x] === 0) grid[y][x] = 255
   else grid[y][x] = 0
 }
 
 function renderGrid() {
+  //Displaying each of the tiles on the screen
   for (let y = 0; y < row; y++) {
     for (let x = 0; x < column; x++) {
       let fillColor = grid[y][x]
@@ -75,20 +118,41 @@ function getCurrentY() {
 }
 
 function mousePressed() {
+  // Checks if the mouse is within the boundaries of the grid
   if (mouseX<width && mouseY < height && !keyIsDown(SHIFT)) {
-
-    let x = getCurrentX()
-    let y = getCurrentY()
+    if (shape === "cross") {
+      let x = getCurrentX()
+      let y = getCurrentY()
     
-    flip(x,y)
-    if (x-1 >= 0) flip(x-1,y)
-    if(y-1 >= 0) flip(x,y-1)
-    if(x + 1 <= grid[0].length) flip(x+1,y)
-    if(y + 1 <= grid.length) flip(x,y+1)
+      flip(x,y)
+      //Checks if the position of the mouse is within the index of the array
+      if (x-1 >= 0) flip(x-1,y)
+      if(y-1 >= 0) flip(x,y-1)
+      if(x + 1 <= grid[0].length) flip(x+1,y)
+      if(y + 1 <= grid.length) flip(x,y+1)
+    }else{
+      let x = getCurrentX()
+      let y = getCurrentY()
+    
+      flip(x,y)
+      if (x-1 >= 0) flip(x-1,y)
+      if(y-1 >= 0) flip(x,y-1)
+      if(y-1 >= 0) flip(x-1,y-1)
+    }
+   
   
-  }else{
+  }else{ //Single tile flip (only when shift is held down)
     let x = getCurrentX()
     let y = getCurrentY()
     flip(x,y)
   }
+}
+
+function keyPressed() {
+  //Changes the current shape 
+  if (key === " ") {
+    if(shape === "cross") shape = "square"
+    else shape = "cross"
+  }
+
 }
